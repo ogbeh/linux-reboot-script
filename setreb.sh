1f72d7e1b47f0abd29076b022bf7f037203c93b1
Sam Ogbeh <sogbeh@gmail.com>
2025-02-19 16:23:34 +0100

    Updated script logic

--- setreb.sh
+++ setreb.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Check if the script is executed with sudo privileges
-if [ "$EUID" -ne 0 ]; then
+if [ "$EUID" -ne 0 ]
+then
   echo "Please run the script with sudo privileges."
   exit 1
 fi
 
-# Define the cron job entry
-CRON_JOB="00 03 * * * root shutdown -r"
+# Define the cron job entry (using explicit LF)
+CRON_JOB="00 03 * * * root /sbin/shutdown -r
+"
 
 # Check if the cron job already exists in /etc/crontab
-if grep -Fxq "$CRON_JOB" /etc/crontab; then
+if grep -Fxq "00 03 * * * root /sbin/shutdown -r" /etc/crontab
+then
   echo "Rebooting schedule already exists."
 else
   # Add the cron job to /etc/crontab
-  echo "$CRON_JOB" >> /etc/crontab
-  if [ $? -eq 0 ]; then
+  printf "%s" "$CRON_JOB" >> /etc/crontab
+  if [ $? -eq 0 ]
+  then
     echo "Rebooting schedule set up successfully."
   else
     echo "Error: Unable to set up rebooting schedule."
